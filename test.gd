b1040ad39a56229c08c7810c1b9fdf26cacd640c
suhyun-kim <suhyun-kim@nhn-commerce.com>
2021-09-30 10:04:05 +0900

    오랜만에 별도 찍어보았다

--- test.gd
+++ test.gd
@@ -5,4 +5,10 @@
     echo "I am ".$age." years old. \n";
     echo "I am a newbie php developer.\n";
 
+    for($i=0; $i <= 5; $i++){
+        for($j = 0; $j <= $i; $j++){
+            echo "*";
+        }
+        echo "\n";
+    }
     exit();
