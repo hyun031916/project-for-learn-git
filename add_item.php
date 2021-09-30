b2e2ae17b7f470b85dd9499277f64723c8397f36
suhyun-kim <suhyun-kim@nhn-commerce.com>
2021-09-30 11:07:29 +0900

    #5 쇼핑몰에서 상품 추가하는 코드 (작년 코드 복붙)

---
+++ add_item.php
@@ -0,0 +1,24 @@
+<?php
+
+include "./db_conn.php";
+include "./main.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<?php
+
+
+?>
+<script>
+    alert("상품이 등록되었습니다.");
+    location.href="shop_list.php";
+</script>
+</html>
